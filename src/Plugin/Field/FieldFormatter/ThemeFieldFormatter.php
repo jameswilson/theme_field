eb9393f5fdfeaf8ede495d3ff64dea6132ffa03f
Vasily Yaremchuk <vaso1977@gmail.com>
2017-05-20 19:24:07 +0300

    Theme filed and Theme negotiator

---
+++ src/Plugin/Field/FieldFormatter/ThemeFieldFormatter.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Drupal\theme_field\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'theme_field_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "theme_field_formatter",
+ *   label = @Translation("Theme"),
+ *   field_types = {
+ *     "theme_field_type"
+ *   }
+ * )
+ */
+class ThemeFieldFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return array(
+      // Implement default settings.
+    ) + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return array(
+      // Implement settings form.
+    ) + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = ['#markup' => $this->viewValue($item)];
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    return '';
+  }
+
+}
---
+++ src/Theme/ThemeFiledNegotiator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\theme_filed\Theme;
+
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Theme\ThemeNegotiatorInterface;
+
+/**
+ * Class ThemeFieldNegotiator.
+ *
+ * @package Drupal\theme_field
+ */
+class ThemeFieldNegotiator implements ThemeNegotiatorInterface {
+
+
+
+}
